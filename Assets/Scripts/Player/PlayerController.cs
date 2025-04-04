f64862d2ae940e9816cc6138da8514049c57a021
Princekum2005 <137779777+Princekum2005@users.noreply.github.com>
2025-04-04 10:23:08 +0530

    PlayerController script added
    PlayerController script - moving player left and right and clamping on x pos

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IMovable, ICollidable
+{
+    private IInputHandler _inputHandler;
+    [SerializeField] private float speed;
+    [SerializeField] private float minX, maxX;
+
+    void Start()
+    {
+        _inputHandler = new KeyboardInput();
+    }
+
+    void Update()
+    {
+        Move(_inputHandler.GetInput());
+    }
+
+    public void Move(Vector2 direction)
+    {
+        Vector3 newPosition = transform.position + (Vector3)(direction * speed * Time.deltaTime);
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        transform.position = newPosition;
+    }
+
+    public void OnCollide()
+    {
+        // GameManager.Instance.GameOver();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            OnCollide();
+        }
+    }
+}
